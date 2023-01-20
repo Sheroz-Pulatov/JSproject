1f8c1aaae9e3dfe2c0448ca99aad5198e4adff01
Sheroz-Pulatov <acauntestudio@gmail.com>
2023-01-20 17:31:52 +0500

    h1 title

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let num = "20"
+
+console.log(
+
+    //parseInt(num) //
+    //parseFloat(num) //
+
+    //Math.ceil(num)// округление вверх
+    //Math.floor(num)// округление в низ
+    //Math.round(num)// логическое округление
+
+    Math.max()
+
+)
